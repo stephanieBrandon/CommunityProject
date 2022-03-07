d68f9a411898dbd6e9759b2f2c1441846818b71e
stephaniebrandon7 <stephaniebrandon01@gmail.com>
2022-03-07 11:24:10 -0500

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import profileImage from './img/profile-pic.JPG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Community Project</h1>
+      <h2>Author: Stephanie Brandon</h2>
+      <img src={profileImage} alt="profile-img" />
+        
+    </div>
+  );
+}
+
+export default App;
